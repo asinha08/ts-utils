ea626ff08de5a2b56eb0d6ecf75324f77736a72e
Ashish Kumar Sinha <ashish_sinha08@outlook.com>
2020-03-20 00:06:54 +0530

    date time formatter

---
+++ tsdate/dateFormatter.go
@@ -0,0 +1,30 @@
+package tsdate
+
+import (
+	"time"
+)
+
+var dateTimeTZFormat = "2006-01-02T15:04:05Z"
+var dateFormat = "2006-01-02"
+
+const timeMilli = int64(time.Millisecond)
+
+func DateTimeTZFormatter(dateTime *time.Time) string {
+	return dateTime.UTC().Format(dateTimeTZFormat)
+}
+
+func DateFormatter(date *time.Time) string {
+	return date.UTC().Format(dateFormat)
+}
+
+func GetDateTimeTZFormat() *string {
+	return &dateTimeTZFormat
+}
+
+func GetDateFormat() *string {
+	return &dateFormat
+}
+
+func GetTimeInMilli(timeValue time.Time) int64 {
+	return int64(timeValue.UnixNano() / timeMilli)
+}
