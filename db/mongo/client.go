61828fe2fd937a4f8ec866bd22e06dcbc14e13d4
Ashish Kumar Sinha <ashish_sinha08@outlook.com>
2020-05-19 20:59:55 +0530

    Mongo db client

---
+++ db/mongo/client.go
@@ -0,0 +1,34 @@
+package mongo
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type DBConfigForMongo struct {
+	UserName string
+	Password string
+	Host     string
+	Port     string
+	AppName  string
+}
+
+var dbClient *mongo.Client
+
+func InitMongoDB(config *DBConfigForMongo) (err error) {
+	connectionString := "mongodb://" + config.UserName + ":" + config.Password + "@" + config.Host + config.Port + "/?appName=" + config.AppName
+	dbClient, err = mongo.Connect(context.Background(), options.Client().ApplyURI(connectionString))
+	return
+}
+
+func GetDB(dbName string) (db *mongo.Database) {
+	db = dbClient.Database(dbName)
+	return
+}
+
+func CloseClient() {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	_ = dbClient.Disconnect(ctx)
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/asinha08/ts-utils
 go 1.13
 
 require (
-	github.com/Shopify/sarama v1.26.1
-	github.com/go-redis/redis v6.15.7+incompatible // indirect
-	github.com/gocql/gocql v0.0.0-20200324094621-6d895e38b0a5 // indirect
-	github.com/golang/protobuf v1.3.4
+	github.com/Shopify/sarama v1.26.4
+	github.com/go-redis/redis v6.15.7+incompatible
+	github.com/gocql/gocql v0.0.0-20200515162754-0714040f3e35
+	github.com/golang/protobuf v1.4.2
+	github.com/onsi/ginkgo v1.12.1 // indirect
+	github.com/onsi/gomega v1.10.0 // indirect
 	github.com/pkg/errors v0.9.1
+	go.mongodb.org/mongo-driver v1.3.3
+	golang.org/x/crypto v0.0.0-20200204104054-c9f3fb736b72
 )
