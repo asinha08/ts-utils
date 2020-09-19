f0b94fb07534aa5a38f1f7c46fc48eb46b274446
Ashish Kumar Sinha <ashish_sinha08@outlook.com>
2020-09-19 05:46:08 +0530

    Json Logger

--- error/tsError.go
+++ error/tsError.go
@@ -1,8 +1,11 @@
 package error
 
 import (
+	"encoding/json"
+	logger "github.com/asinha08/ts-utils/tslogger/fluentd"
 	"github.com/asinha08/ts-utils/tsprotos"
 	"github.com/golang/protobuf/proto"
+	"net/http"
 )
 
 func GetError(code string, message string) []byte {
@@ -12,4 +15,37 @@ func GetError(code string, message string) []byte {
 	}
 	b, _ := proto.Marshal(errResponse)
 	return b
+}
+
+type JsonError struct {
+	Err       error
+	Code      string
+	Status    int
+	ExtraLogs map[string]interface{}
+}
+
+type errorMessage struct {
+	Err  string `json:"err"`
+	Code string `json:"code"`
+}
+
+func GetJsonError(err *JsonError, w http.ResponseWriter, r *http.Request) {
+	logs := map[string]interface{}{
+		"errorCode":    err.Code,
+		"errorMessage": err.Err.Error(),
+	}
+	if len(err.ExtraLogs) > 0 {
+		for index, item := range err.ExtraLogs {
+			logs[index] = item
+		}
+	}
+	logger.LogError(logs, r)
+
+	w.WriteHeader(err.Status)
+	res := &errorMessage{
+		Err:  err.Err.Error(),
+		Code: err.Code,
+	}
+	out, _ := json.Marshal(res)
+	_, _ = w.Write(out)
 }
