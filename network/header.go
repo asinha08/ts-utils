10968abd2be646a2941b40888607a1bae8334896
Ashish Kumar Sinha <ashish_sinha08@outlook.com>
2022-01-28 19:58:33 +0530

    network request header reader added

--- network/header.go
+++ network/header.go
@@ -5,7 +5,7 @@ import "net/http"
 func Reader(r *http.Request) (clientLang string, extraLogs map[string]interface{}) {
 	clientLang = r.Header.Get("language")
 	correlationId := r.Header.Get("x-correlation-id")
-
+	extraLogs = make(map[string]interface{})
 	extraLogs["clientLang"] = clientLang
 	extraLogs["correlationId"] = correlationId
 	return clientLang, extraLogs
