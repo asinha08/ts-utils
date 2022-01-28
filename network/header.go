bf4feb898506dbf49386309abe727ee2a56a6317
Ashish Kumar Sinha <ashish_sinha08@outlook.com>
2022-01-28 20:43:59 +0530

    Request header has wrong key name for accept-language

--- network/header.go
+++ network/header.go
@@ -3,10 +3,10 @@ package network
 import "net/http"
 
 func Reader(r *http.Request) (clientLang string, extraLogs map[string]interface{}) {
-	clientLang = r.Header.Get("language")
+	clientLang = r.Header.Get("accept-language")
 	correlationId := r.Header.Get("x-correlation-id")
 	extraLogs = make(map[string]interface{})
-	extraLogs["clientLang"] = clientLang
-	extraLogs["correlationId"] = correlationId
+	extraLogs["accept-language"] = clientLang
+	extraLogs["x-correlation-id"] = correlationId
 	return clientLang, extraLogs
 }
